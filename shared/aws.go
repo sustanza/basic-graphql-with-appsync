cac6728369681d744472588f2b7218c467614d44
Sustanza <422661+sustanza@users.noreply.github.com>
2020-06-19 15:54:33 -0500

    Fixes and updates to code

--- shared/aws.go
+++ shared/aws.go
@@ -1,10 +1,12 @@
 package shared
 
 import (
+	"log"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"log"
 )
+
 func GetAWSSession() (*session.Session, error) {
 	sess, err := session.NewSession(&aws.Config{})
 
